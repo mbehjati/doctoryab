d1c2b4b7088bc8d29da05656904403e68c2d077c
Masomeh <m.soleimani73.z@gmail.com>
2016-12-24 20:34:43 +0330

    fix media  urls

--- doctoryab/urls.py
+++ doctoryab/urls.py
@@ -13,7 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from appointment import views
@@ -25,3 +28,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^user/', include('user.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
