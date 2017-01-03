7c835560850386b56c37234bd1ab40eca425aaf5
Masomeh <m.soleimani73.z@gmail.com>
2017-01-03 09:41:33 +0330

    translate user model error messages

--- user/models.py
+++ user/models.py
@@ -21,10 +21,10 @@ class MyUser(models.Model):
     user = models.OneToOneField(User)
     phone_regex = RegexValidator(regex=r'^([0]{1})([0-9]{10})$',
                                  # r'^\+?1?\d{9,15}$',
-                                 message="Phone number must be entered in the format: '09999999999'. 11 digits allowed.")
+                                 message="شماره تلفن وارد شده باید 11 رقمی بوده و با صفر شروع شود و با ارقام انگلیسی باشد.")
     phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=11)
     national_code_regex = RegexValidator(regex=r'^([0-9]{10})$',
-                                         message="national code must be 10 digits.")
+                                         message="کدملی وارد شده باید 10 رقمی و با ارقام انگلیسی باشد.")
     national_code = models.CharField(validators=[national_code_regex], blank=True, max_length=10)
 
     is_active = True
@@ -38,12 +38,12 @@ class Doctor(models.Model):
     user = models.OneToOneField(MyUser, default='1')
     university = models.CharField(max_length=60)
     year_diploma_regex = RegexValidator(regex=r'([0-9]{4})',
-                                        message="year must be 4 digits")
+                                        message="سال وارد شده باید 4 رقمی و با ارقام انگلیسی باشد.")
     year_diploma = models.CharField(validators=[year_diploma_regex], blank=True, max_length=4)
     diploma = models.CharField(max_length=50)
     office_address = models.CharField(max_length=300)
     phone_regex = RegexValidator(regex=r'^([0]{1})([0-9]{10})$',
-                                 message="Phone number must be entered in the format: '09999999999'.")
+                                 message="شماره تلفن وارد شده باید 11 رقمی بوده و با صفر شروع شود و با ارقام انگلیسی باشد.")
     office_phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=11)
     insurance = models.ManyToManyField(Insurance)
     expertise = models.ForeignKey(Expertise)
