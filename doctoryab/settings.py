ec9055abc135114241ce8b59f46b2c94620d68da
mina-rf <rafiei.mina73@gmail.com>
2017-01-08 19:55:55 +0330

    Refactor

--- doctoryab/settings.py
+++ doctoryab/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -86,6 +88,9 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -128,6 +133,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_ROOT = '/Users/a/Documents/ASD/doctoryab/media/'
--- user/views.py
+++ user/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, HttpResponse
 
 from appointment.logic.doctor_plan import get_doctor_day_plan
-from appointment.logic.jalali import Gregorian
+from lib.jalali import Gregorian
 from user.forms import *
 from user.models import *
 from .doctor_plan import save_doctor_free_times_in_db
