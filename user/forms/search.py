c1d0be56dfce4513fe7f9c543070af2ad8bc2980
mina-rf <rafiei.mina73@gmail.com>
2017-01-09 12:51:36 +0330

    Test

--- user/forms/search.py
+++ user/forms/search.py
@@ -1,22 +1,20 @@
 from django import forms
 
-from user.models import Insurance, Expertise
-
 
 class AdvancedSearchForm(forms.Form):
-    insurance_choices = (('همه', 'همه'),)
-    expertise_choices = (('همه', 'همه'),)
-
-    for ins in Insurance.objects.all():
-        insurance_choices += ((ins, ins.name),)
-
-    for exp in Expertise.objects.all():
-        expertise_choices += ((exp, exp.name),)
+    # insurance_choices = (('همه', 'همه'),)
+    # expertise_choices = (('همه', 'همه'),)
+    #
+    # for ins in Insurance.objects.all():
+    #     insurance_choices += ((ins, ins.name),)
+    #
+    # for exp in Expertise.objects.all():
+    #     expertise_choices += ((exp, exp.name),)
 
     name = forms.CharField(label='نام پزشک', required=False)
-    expertise = forms.ChoiceField(choices=expertise_choices, label='تخصص')
+    # expertise = forms.ChoiceField(choices=expertise_choices, label='تخصص')
     date = forms.CharField(label='تاریخ', required=False)
-    insurance = forms.ChoiceField(choices=insurance_choices, label='بیمه')
+    # insurance = forms.ChoiceField(choices=insurance_choices, label='بیمه')
     address = forms.CharField(label='آدرس', required=False)
 
     def clean(self):
