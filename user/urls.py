5d04b526ccbdae2c50d1da889dc02ba98667b9b5
Masomeh <m.soleimani73.z@gmail.com>
2017-01-10 23:35:43 +0330

    Add comments to user app

--- user/urls.py
+++ user/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     url(r'^register$', views.register, name='register'),
     url(r'^login$', views.login, name='login'),
     url(r'^logout$', views.logout, name='logout'),
-    url(r'^profile', views.view_profile, name='viewProfile'),
     url(r'^edit-profile$', views.edit_profile, name='EditProfile'),
     url(r'^edit-password$', views.edit_password, name='EditPassword'),
     url(r'^enter-plan$', views.doctor_free_time, name='EnterPlan'),
