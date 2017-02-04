373721a8154779c29b0304bfc9142cf7e0534b0b
mina-rf <rafiei.mina73@gmail.com>
2017-02-04 18:36:15 +0330

    Fix appointment sort

--- appointment/logic/appointment_time.py
+++ appointment/logic/appointment_time.py
@@ -3,7 +3,6 @@
 
 def is_time_before(first, second):
     """
-
     :param first: first time
     :param second: second time
     :return: true if first time is before second one (in 12 hours format)
@@ -16,27 +15,29 @@ def is_time_before(first, second):
         return True
     if '12:' in first and '12:' not in second:
         return True
+    if '12:' in second and '12:' not in first:
+        return False
     if first > second:
         return False
     return True
 
 
 def sort_appointment_times_in_day(apps):
-    for i in range(len(apps)):
-        for j in range(len(apps)):
-            if is_time_before(apps[i].start_time, apps[j].start_time):
-                apps[i], apps[j] = apps[j], apps[i]
+    for j in range(len(apps)):
+        for i in range(len(apps) - j - 1):
+            if is_time_before(apps[i + 1].start_time, apps[i].start_time):
+                apps[i + 1], apps[i] = apps[i], apps[i + 1]
     return apps
 
 
 def sort_appointment_times(apps):
     for i in range(len(apps)):
-        for j in range(len(apps)):
-            if apps[i].date == apps[j].date:
-                if is_time_before(apps[i].start_time, apps[j].start_time):
-                    apps[i], apps[j] = apps[j], apps[i]
-            elif apps[i].date < apps[j].date:
-                apps[i], apps[j] = apps[j], apps[i]
+        for j in range(len(apps) - i - 1):
+            if apps[j].date == apps[j + 1].date:
+                if is_time_before(apps[j + 1].start_time, apps[j].start_time):
+                    apps[j + 1], apps[j] = apps[j], apps[j + 1]
+            elif apps[j + 1].date < apps[j].date:
+                apps[j + 1], apps[j] = apps[j], apps[j + 1]
     return apps
 
 
@@ -64,6 +65,7 @@ def cluster_appointment_times(apps):
 def add_time(start_time, duration):
     duration = int(duration)
     hour = int(start_time.split(':')[0])
+    # TODO: What about :-2?
     minute = int(start_time.split(':')[1][0:len(start_time.split(':')[1]) - 2])
     postfix = start_time[len(start_time) - 2:len(start_time)]
 
