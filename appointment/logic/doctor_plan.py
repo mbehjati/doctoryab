22de50e3f264298b8528525ce9fa0a86bf0f1e3a
mina-rf <rafiei.mina73@gmail.com>
2017-02-04 19:55:56 +0330

    Add function for sending mail to doctor after making an appointment

--- appointment/logic/doctor_plan.py
+++ appointment/logic/doctor_plan.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+from django.core.mail import send_mail
 
 from appointment.logic.appointment_time import *
 from appointment.logic.appointment_time import sort_appointment_times_in_day
@@ -11,11 +12,21 @@ def get_doctor_all_plan(start_date, doctor):
     :param doctor: doctor
     :return: list of doctor plan from a given time to end
     """
-    apps = [app for app in AppointmentTime.objects.filter(doctor=doctor) if app.date >= start_date]
+    apps = [app for app in AppointmentTime.objects.filter(doctor=doctor) if
+            (app.date >= start_date and app.confirmation != '2')]
 
     apps = sort_appointment_times(apps)
     return cluster_appointment_times(apps)
 
 
 def get_doctor_day_plan(date, doctor):
-    return sort_appointment_times_in_day(list(AppointmentTime.objects.filter(doctor=doctor, date=date)))
+    return sort_appointment_times_in_day(
+        list(AppointmentTime.objects.filter(doctor=doctor, date=date, confirmation__in=['1', '3'])))
+
+
+def send_app_reserve_mail(app):
+    title = 'نوبت جدید توسط بیمار'
+    body = ' با سلام \n بیمار' + str(app.patient.user.first_name) + ' ' + str(
+        app.patient.user.last_name) + ' در تاریخ ' + str(app.date) + ' ساعت  ' + str(
+        app.start_time) + 'از شما تقاضای نوبت کرده است. لطفا در صورت موافقت این نوبت را تایید فرمایید.'
+    send_mail(title, body, 'onlinefoodforyou@gmail.com', [app.doctor.user.user.email], fail_silently=False)
