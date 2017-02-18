7c9e7675d432dae6233332e69f0f74ff7c797816
Masomeh <m.soleimani73.z@gmail.com>
2017-02-18 16:39:19 +0330

    Refactor register form using merge class, extract method

--- user/forms/__init__.py
+++ user/forms/__init__.py
@@ -1,3 +1,3 @@
 from user.forms.login import LoginForm
-from user.forms.register import UserForm, MyUserForm, DoctorForm
+from user.forms.register import DoctorForm, UserForm
 from user.forms.editpassword import EditPasswordForm
--- user/forms/register.py
+++ user/forms/register.py
@@ -1,24 +1,28 @@
 # -*- coding: UTF-8 -*-
 from django import forms
 from django.contrib.auth.models import User
-
 from user.models import MyUser, Doctor
 
 
 class UserForm(forms.ModelForm):
-    password2 = forms.CharField(widget=forms.widgets.PasswordInput, label='تکرار گذرواژه')
+    username = forms.CharField(max_length=100, label='نام‌کاربری', required=True)
+    password = forms.CharField(widget=forms.widgets.PasswordInput, label=' گذرواژه', required=True)
+    password2 = forms.CharField(widget=forms.widgets.PasswordInput, label='تکرار گذرواژه', required=True)
+    first_name = forms.CharField(max_length=100, label='نام', required=False)
+    last_name = forms.CharField(max_length=100, label='نام خانواگی', required=False)
+    email = forms.EmailField(max_length=100, label='ایمیل', required=False)
 
     def __init__(self, *args, **kwargs):
-        delete_some_field = kwargs.get('delete_some_field', False)
-        if 'delete_some_field' in kwargs:
-            del kwargs['delete_some_field']
+        self.register_form = kwargs.pop('for_register', False)
+        self.edit_form = kwargs.pop('for_edit_profile', False)
         super(UserForm, self).__init__(*args, **kwargs)
-        if delete_some_field:
+        if self.edit_form:
             del self.fields['username']
             del self.fields['password']
             del self.fields['password2']
-            # or
-            # self.fields['is_manager'].widget = something_else
+            self.fields['first_name'].initial = self.instance.user.first_name
+            self.fields['last_name'].initial = self.instance.user.last_name
+            self.fields['email'].initial = self.instance.user.email
 
     def clean(self):
         cleaned_data = super(UserForm, self).clean()
@@ -28,43 +32,67 @@ class UserForm(forms.ModelForm):
 
         if _password2 and _password and _password2 != _password:
             raise forms.ValidationError('تکرار گذرواژه اشتباه است.')
+        if User.objects.filter(username=cleaned_data.get('username')).count():
+            raise forms.ValidationError('نام کاربری قبلاً ثبت شده است.')
         return cleaned_data
 
     class Meta:
-        model = User
-        fields = ['username', 'password', 'password2', 'first_name', 'last_name', 'email']
+        model = MyUser
+        exclude = ['user', 'is_doctor']
+        fields = ['username', 'password', 'password2', 'first_name', 'last_name',
+                  'email', 'national_code', 'phone_number', 'image']
         widgets = {
             'password': forms.PasswordInput,
             'password2': forms.PasswordInput,
-            'email': forms.EmailInput
+            'email': forms.EmailInput,
+            'image': forms.FileInput
         }
-        labels = {
-            'username': 'نام‌کاربری',
-            'password': 'گذواژه',
-            'password2': 'تکرار گذرواژه',
-            'first_name': 'نام',
-            'last_name': 'نام خانواگی',
-            'email': 'ایمیل'
-        }
-
-
-class MyUserForm(forms.ModelForm):
-    class Meta:
-        model = MyUser
-        exclude = ['user', 'is_doctor']
         labels = {
             'phone_number': "شماره تلفن",
             'national_code': "کد ملی"
         }
 
+    def save(self, commit=True, *args, **kwargs):
+        user = kwargs.get('user')
+        # existed_my_user = kwargs.get('my_user')
+        my_user = super(UserForm, self).save(commit=False)
+
+        if self.register_form:
+            my_user = self.register_my_user(my_user=my_user)
+            if commit:
+                my_user.save()
+            return my_user
+
+        elif self.edit_form:
+            self.edit_my_user(user=user)
+            if commit:
+                self.instance.save()
+            return self.instance
+
+    def register_my_user(self, my_user):
+        user = User.objects.create_user(username=self.cleaned_data['username'],
+                                        password=self.cleaned_data['password'],
+                                        first_name=self.cleaned_data['first_name'],
+                                        last_name=self.cleaned_data['last_name'],
+                                        email=self.cleaned_data['email'])
+        user.is_active = True
+        my_user.user = user
+        return my_user
+
+    def edit_my_user(self, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+        user.save()
+        self.instance.user = user
+
 
 class DoctorForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
-        delete_some_field = kwargs.get('delete_some_field', False)
-        if 'delete_some_field' in kwargs:
-            del kwargs['delete_some_field']
+        self.register_form = kwargs.pop('for_register', False)
+        self.edit_form = kwargs.pop('for_edit_profile', False)
         super(DoctorForm, self).__init__(*args, **kwargs)
-        if delete_some_field:
+        if self.edit_form:
             del self.fields['contract']
 
     class Meta:
@@ -80,3 +108,26 @@ class DoctorForm(forms.ModelForm):
             'expertise': 'تخصص',
             'insurance': 'بیمه',
         }
+
+    def save(self, commit=True, *args, **kwargs):
+        user = kwargs.get('user')
+        doctor = super(DoctorForm, self).save(commit=False)
+
+        if self.register_form:
+            doctor = self.register_doctor(user=user, doctor=doctor)
+            doctor.user = user
+            doctor.save()
+            for ins in self.cleaned_data['insurance']:
+                doctor.insurance.add(ins)
+
+            if commit:
+                doctor.save()
+            return doctor
+
+        elif self.edit_form:
+            self.instance.insurance = self.cleaned_data['insurance']
+            self.instance.user = user
+
+            if commit:
+                self.instance.save()
+            return self.instance
