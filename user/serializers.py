7a5dc7c65fc7470062e389124137b125136b085b
melika <melikabehjati@gmail.com>
2017-02-18 23:59:37 +0330

    Fix advanced search form serializer problem for codeship

--- user/serializers.py
+++ user/serializers.py
@@ -22,19 +22,19 @@ class MyUserSerializer(ModelSerializer):
         depth = 2
 
 
-class SearchFormSerializer(serializers.Serializer):
-    insurance_choices = (('همه', 'همه'),)
-    expertise_choices = (('همه', 'همه'),)
-    try:
-        for ins in Insurance.objects.all():
-            insurance_choices += ((ins, ins.name),)
-
-        for exp in Expertise.objects.all():
-            expertise_choices += ((exp, exp.name),)
-    except OperationalError:
-        pass
-    name = serializers.CharField(label='نام پزشک', required=False)
-    expertise = serializers.ChoiceField(choices=expertise_choices, label='تخصص')
-    date = serializers.CharField(label='تاریخ', required=False)
-    insurance = serializers.ChoiceField(choices=insurance_choices, label='بیمه')
-    address = serializers.CharField(label='آدرس', required=False)
+# class SearchFormSerializer(serializers.Serializer):
+#     insurance_choices = (('همه', 'همه'),)
+#     expertise_choices = (('همه', 'همه'),)
+#     try:
+#         for ins in Insurance.objects.all():
+#             insurance_choices += ((ins, ins.name),)
+#
+#         for exp in Expertise.objects.all():
+#             expertise_choices += ((exp, exp.name),)
+#     except OperationalError:
+#         pass
+#     name = serializers.CharField(label='نام پزشک', required=False)
+#     expertise = serializers.ChoiceField(choices=expertise_choices, label='تخصص')
+#     date = serializers.CharField(label='تاریخ', required=False)
+#     insurance = serializers.ChoiceField(choices=insurance_choices, label='بیمه')
+#     address = serializers.CharField(label='آدرس', required=False)
