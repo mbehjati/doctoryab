14887ca3d3e3dcb7a2ee99ba8f349daf0a21340d
Masomeh <m.soleimani73.z@gmail.com>
2017-02-23 03:04:31 +0330

    Change doctor model to fix codeship error

--- user/models.py
+++ user/models.py
@@ -68,8 +68,8 @@ class Doctor(models.Model):
     expertise = models.ForeignKey(Expertise)
     contract = models.FileField(upload_to='contracts')
 
-    lat_location = models.FloatField(blank=True, default=None)
-    lon_location = models.FloatField(blank=True, default=None)
+    lat_location = models.FloatField(blank=True, null=True, default=None)
+    lon_location = models.FloatField(blank=True, null=True, default=None)
 
     def __str__(self):
         return str(self.user.user.username)
