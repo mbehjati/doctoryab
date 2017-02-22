c3d04d8e97c12ddbcab4ca41f6d38e749ebcd18a
melika <melikabehjati@gmail.com>
2017-02-22 17:20:46 +0330

    Refactor rename weeklyPlanSerializer to a more general name

--- user/serializers.py
+++ user/serializers.py
@@ -4,23 +4,35 @@ from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from appointment.serializers import AppointmentSerializer
-from user.models import Doctor, MyUser
+from user.models import Doctor, MyUser, Insurance, Expertise
 
 
 class DoctorSerializer(ModelSerializer):
     class Meta:
         model = Doctor
-        fields = ('user', 'diploma', 'office_address', 'office_phone_number', 'expertise', 'insurance')
+        fields = ('user', 'diploma', 'office_address', 'office_phone_number', 'expertise', 'insurance', 'title')
         depth = 3
 
 
 class MyUserSerializer(ModelSerializer):
     class Meta:
         model = MyUser
-        fields = '__all__'  # TODO: see what we need and we dont
+        fields = '__all__'  # TODO: see what we need and we don't
         depth = 2
 
 
-class WeeklyPlanSerializer(serializers.Serializer):
+class DateAppointmentSerializer(serializers.Serializer):
     date = serializers.CharField(max_length=10)
     appointments = AppointmentSerializer(many=True)
+
+
+class InsuranceSerializer(ModelSerializer):
+    class Meta:
+        model = Insurance
+        fields = ('name', '')
+
+
+class ExpertiseSerializer(ModelSerializer):
+    class Meta:
+        model = Expertise
+        fields = ('name',)
